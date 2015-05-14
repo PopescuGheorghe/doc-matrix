22403e5799927bef1131048dc51357fb10f8c831
PopescuGheorghe <gimmy.xdd@gmail.com>
2015-05-15 02:19:45 +0300

    Added devise and assigned documents to users

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,2 +1,10 @@
 class Document < ActiveRecord::Base
+  belongs_to :user
+
+  # ensure that a user_id is present
+  validates :user_id, presence: true
+  # ensure that title is present and at least 6 chars long
+  validates :title, length: { minimum: 6 }, presence: true
+  # ensure that content is present and at between 10 and 10000 chars long
+  validates :content, length: { minimum: 10, maximum: 10_000 }, presence: true
 end
