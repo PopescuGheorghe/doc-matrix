dda374c0f7421e616d71c5ce6f6b4abebd7721b2
PopescuGheorghe <gimmy.xdd@gmail.com>
2015-05-15 02:45:32 +0300

    added bootstrap

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,8 +3,8 @@ class Document < ActiveRecord::Base
 
   # ensure that a user_id is present
   validates :user_id, presence: true
-  # ensure that title is present and at least 6 chars long
-  validates :title, length: { minimum: 6 }, presence: true
+  # ensure that title is present and at least 2 chars long
+  validates :title, length: { minimum: 2 }, presence: true
   # ensure that content is present and at between 10 and 10000 chars long
   validates :content, length: { minimum: 10, maximum: 10_000 }, presence: true
 end
