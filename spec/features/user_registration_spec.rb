3c6e198205849f9c8c4cc991e71369ec7cd484d3
PopescuGheorghe <gimmy.xdd@gmail.com>
2015-06-15 18:15:53 +0300

    corected typo

--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'User registraters' do
+feature 'User registers' do
   scenario 'with valid credentials' do
     sign_up_with 'test@mail.com', 'password'
     expect(page).to have_content("Welcome! You have signed up successfully.")
